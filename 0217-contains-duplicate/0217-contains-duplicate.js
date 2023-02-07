0ea062cfee2898d6fa0c75d9145b7b7ec4819976
SHWsgithub <111102006+SHWsgithub@users.noreply.github.com>
2023-02-07 22:20:41 +0900

    Time: 95 ms (71.15%), Space: 59.3 MB (5.03%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,5 +3,13 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    return new Set(nums).size !== nums.length;
+    let map = new Map();
+    
+    for(let i = 0; i < nums.length; i++) {
+        if(map.has(nums[i])) {
+            return true;
+        }
+        map.set(nums[i], i);
+    }
+    return false;
 };
