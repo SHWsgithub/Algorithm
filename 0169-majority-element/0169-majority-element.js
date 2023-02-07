80d8bc5ca6510318b5ba908c2c85e63bcaa3b3c9
SHWsgithub <111102006+SHWsgithub@users.noreply.github.com>
2023-02-07 21:53:08 +0900

    Time: 75 ms (59.21%), Space: 43.1 MB (87.97%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,6 +3,14 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    nums.sort();
-    return nums[Math.floor(nums.length / 2)];
+    let count = 0;
+    let expect = null;
+    
+    for (let i = 0; i < nums.length; i++) {
+        if(count === 0) {
+            expect = nums[i];
+        }
+        nums[i] === expect ? count++ : count--;
+    }
+    return expect;
 };
