c906e66e0cf91b92abc13b92a99ba9519952a31e
SHWsgithub <111102006+SHWsgithub@users.noreply.github.com>
2023-02-13 16:27:04 +0900

    Time: 71 ms (99.17%), Space: 52.1 MB (11.64%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    if (!prices.length) return 0;
+
+    let minPrice = prices[0];
+    let maxProfit = 0;
+
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+             minPrice = prices[i];
+        } 
+        else {
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+        }
+    }
+
+    return maxProfit;
+};
