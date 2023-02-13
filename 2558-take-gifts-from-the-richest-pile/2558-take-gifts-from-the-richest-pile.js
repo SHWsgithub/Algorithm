e07f6d81e8d587454fc0449a89a81b77413d3bc9
SHWsgithub <111102006+SHWsgithub@users.noreply.github.com>
2023-02-13 09:51:05 +0900

    Time: 116 ms (45.48%), Space: 45.9 MB (52.19%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.js
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.js
@@ -3,12 +3,22 @@
  * @param {number} k
  * @return {number}
  */
-var pickGifts = function (gifts, k) {
-  for (let i = 0; i < k; i++) {
-    gifts.sort((a, b) => b - a);
-    gifts.push(Math.floor(Math.sqrt(gifts[0])));
-    gifts = gifts.slice(1);
-  }
-
-  return gifts.reduce((a, b) => a + b);
+var pickGifts = function(gifts, k) {
+    const q = new MaxPriorityQueue();
+    for (const i of gifts) {
+        q.enqueue(i);
+    }
+    
+    while (k--) {
+        const val = q.dequeue().element;
+        const fsqrt = Math.floor(Math.sqrt(val));
+        q.enqueue(fsqrt);
+    }
+    
+    let sum = 0;
+   while (!q.isEmpty()) {
+       sum += q.dequeue().element;
+   }
+    
+    return sum;
 };
