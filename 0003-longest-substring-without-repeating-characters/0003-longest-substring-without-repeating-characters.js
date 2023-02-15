d441413854b579cabb8dde1737b6224c479cafc2
SHWsgithub <111102006+SHWsgithub@users.noreply.github.com>
2023-02-15 09:47:57 +0900

    Time: 78 ms (95.21%), Space: 44.8 MB (93.49%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,20 +3,16 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-  const n = s.length;
-  let maxLen = 0;
-  let left = 0;
-  const seen = {};
+  let start = 0, maxLen = 0;
+  const map = new Map();
 
-  for (let right = 0; right < n; right++) {
-    if (s[right] in seen) {
-      // if s[right] is a repeat character, move left pointer to the next index of the first occurrence
-      left = Math.max(left, seen[s[right]] + 1);
-    }
-    // update the index of the most recent occurrence of s[right]
-    seen[s[right]] = right;
-    // update the maximum length of non-repeating characters
-    maxLen = Math.max(maxLen, right - left + 1);
+  for(var i = 0; i < s.length; i++) {
+      let ch = s[i];
+    
+      if(map.get(ch) >= start) start = map.get(ch) + 1;
+      map.set(ch, i);
+    
+      if(i - start + 1 > maxLen) maxLen = i - start + 1;
   }
 
   return maxLen;
