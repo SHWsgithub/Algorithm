0fbfc1e2e1c9b9ee47be79b7b98dd99c43d5f13d
SHWsgithub <111102006+SHWsgithub@users.noreply.github.com>
2023-02-15 09:51:52 +0900

    Time: 86 ms (84.73%), Space: 45 MB (92.21%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -6,13 +6,15 @@ var lengthOfLongestSubstring = function(s) {
   let start = 0, maxLen = 0;
   const map = new Map();
 
-  for(var i = 0; i < s.length; i++) {
+  for(let i = 0; i < s.length; i++) {
+      
       let ch = s[i];
     
       if(map.get(ch) >= start) start = map.get(ch) + 1;
+      
       map.set(ch, i);
     
-      if(i - start + 1 > maxLen) maxLen = i - start + 1;
+      maxLen = Math.max(maxLen, i - start + 1)
   }
 
   return maxLen;
