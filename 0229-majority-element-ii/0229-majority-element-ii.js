200472d5a68e8d4cd3bb92c5197029e680c3968f
SHWsgithub <111102006+SHWsgithub@users.noreply.github.com>
2023-02-20 17:14:58 +0900

    Time: 74 ms (52.17%), Space: 42.8 MB (89.03%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.js
+++ 0229-majority-element-ii/0229-majority-element-ii.js
@@ -3,14 +3,38 @@
  * @return {number[]}
  */
 var majorityElement = function(nums) {
-    let map = new Map();
-    const result = [];
+    const n = nums.length
+    if (n < 1) return []
+    if (n < 2) return nums
     
-    for (let num of nums) {
-        map.set(num, (map.get(num) || 0) + 1);
+    let count1 = 0, count2 = 0, candidate1 = 0, candidate2 = 1
+    
+    for (let i = 0; i < n; i++) {
+        if (nums[i] == candidate1) count1++
+        else if (nums[i] == candidate2) count2++
+        else if (count1 == 0) {
+            candidate1 = nums[i]
+            count1 = 1
+        } 
+        else if (count2 == 0) {
+            candidate2 = nums[i]
+            count2 = 1
+        } else {
+            count1--
+            count2--
+        }
     }
     
-    return Array.from(map.entries())
-        .filter(([num, count]) => count > Math.floor(nums.length / 3))
-        .map(([num]) => num);
+    let x = []
+    if (nums.count(candidate1) > n / 3) x.push(candidate1)
+    if (nums.count(candidate2) > n / 3) x.push(candidate2)
+        
+    return x
 };
+Array.prototype.count = function(num) {
+    let count = 0
+    for (let i = 0; i < this.length; i++)
+        if (this[i] == num) count++
+    
+    return count
+}
