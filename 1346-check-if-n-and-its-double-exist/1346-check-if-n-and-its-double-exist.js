5327adb7f88908ac9754399f37d97ae122b25619
SHWsgithub <111102006+SHWsgithub@users.noreply.github.com>
2023-02-20 14:06:46 +0900

    Time: 56 ms (96.84%), Space: 42.3 MB (67.44%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.js
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.js
@@ -3,11 +3,12 @@
  * @return {boolean}
  */
 var checkIfExist = function(arr) {
-    for(let i = 0; i < arr.length - 1; i++) {
-        for(let j = 0; j < arr.length; j++ ) {
-            if(i === j) continue;
-            if(arr[i] === arr[j] * 2) return true;
+    const map = new Map();
+    for(let i = 0; i < arr.length; i++) {
+        if(map.has(arr[i] * 2) || (arr[i] % 2 === 0 && map.has(arr[i] / 2))) {
+            return true;
         }
+        map.set(arr[i], i);
     }
     return false;
 };
