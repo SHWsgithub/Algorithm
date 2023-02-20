42af8986ae7708bfa9c1cf432688e6a9fe0b9631
SHWsgithub <111102006+SHWsgithub@users.noreply.github.com>
2023-02-20 16:29:22 +0900

    Time: 92 ms (96.27%), Space: 60 MB (13.84%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
@@ -4,5 +4,17 @@
  * @return {number}
  */
 var findKthLargest = function(nums, k) {
-    return nums.sort((a,b) => b-a)[k-1]
+    const pivotIndex = Math.floor(Math.random() * nums.length);
+    const pivot = nums[pivotIndex];
+    const left = [], right = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        if (i === pivotIndex) continue;
+        if (nums[i] >= pivot) left.push(nums[i]);
+        else right.push(nums[i]);
+    }
+
+    if (left.length === k - 1) return pivot;
+    else if (left.length >= k) return findKthLargest(left, k);
+    else return findKthLargest(right, k - left.length - 1);
 };
