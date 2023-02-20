d5c95c722fce44372f20de2926111f7719036d69
SHWsgithub <111102006+SHWsgithub@users.noreply.github.com>
2023-02-20 14:57:38 +0900

    Time: 71 ms (58.60%), Space: 43.7 MB (77.40%) - LeetHub

--- 0179-largest-number/0179-largest-number.js
+++ 0179-largest-number/0179-largest-number.js
@@ -8,14 +8,8 @@ var largestNumber = function(nums) {
     nums.sort((a, b) => {
         let exam1 = a + b;
         let exam2 = b + a;
-        for(let i = 0; i < exam1.length; i++) {
-            if(exam1[i] > exam2[i]) {
-                return -1;
-            } else if (exam1[i] < exam2[i]) {
-                return 1;
-            }
-        }
-        return 0;
+        if(exam1 > exam2) return -1;
+        else return 1;        
     });
 
     if (nums[0] === '0') {
