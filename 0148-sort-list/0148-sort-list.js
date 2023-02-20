ebb0557eb5236a34acd14f4e9676d0f302cf0fc4
SHWsgithub <111102006+SHWsgithub@users.noreply.github.com>
2023-02-20 14:34:35 +0900

    Time: 202 ms (75.66%), Space: 63.3 MB (90.46%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var sortList = function(head) {
+    if (!head || !head.next) {
+        return head;
+    }
+    
+    let sortList = [];
+    let cur = head;
+    while (cur) {
+        sortList.push(cur.val);
+        cur = cur.next;
+    }
+    
+    sortList.sort((a, b) => a - b);
+    
+    cur = head;
+    for (let i = 0; i < sortList.length; i++) {
+        cur.val = sortList[i];
+        cur = cur.next;
+    }
+    
+    return head;
+};
