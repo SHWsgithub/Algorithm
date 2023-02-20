ed446fc382b78b8646f9d60657d4734163b183da
SHWsgithub <111102006+SHWsgithub@users.noreply.github.com>
2023-02-20 17:16:11 +0900

    Time: 72 ms (61.70%), Space: 42.7 MB (94.41%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.js
+++ 0229-majority-element-ii/0229-majority-element-ii.js
@@ -3,38 +3,14 @@
  * @return {number[]}
  */
 var majorityElement = function(nums) {
-    const n = nums.length
-    if (n < 1) return []
-    if (n < 2) return nums
-    
-    let count1 = 0, count2 = 0, candidate1 = 0, candidate2 = 1
-    
-    for (let i = 0; i < n; i++) {
-        if (nums[i] == candidate1) count1++
-        else if (nums[i] == candidate2) count2++
-        else if (count1 == 0) {
-            candidate1 = nums[i]
-            count1 = 1
-        } 
-        else if (count2 == 0) {
-            candidate2 = nums[i]
-            count2 = 1
-        } else {
-            count1--
-            count2--
-        }
+    const memo = {}
+    for (let i = 0; i < nums.length; i++) {
+      if (!memo[nums[i]]) memo[nums[i]] = 0
+      memo[nums[i]]++
     }
-    
-    let x = []
-    if (nums.count(candidate1) > n / 3) x.push(candidate1)
-    if (nums.count(candidate2) > n / 3) x.push(candidate2)
-        
-    return x
+    const ret = []
+    for (const m in memo) {
+      if (memo[m] > nums.length / 3) ret.push(parseInt(m))
+    }
+    return ret
 };
-Array.prototype.count = function(num) {
-    let count = 0
-    for (let i = 0; i < this.length; i++)
-        if (this[i] == num) count++
-    
-    return count
-}
