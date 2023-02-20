064c3d587bbdb5e4defb7fbc3f4ace94028bda15
SHWsgithub <111102006+SHWsgithub@users.noreply.github.com>
2023-02-20 17:22:00 +0900

    Time: 77 ms (39.75%), Space: 42.9 MB (89.03%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.js
+++ 0229-majority-element-ii/0229-majority-element-ii.js
@@ -2,21 +2,12 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+
 var majorityElement = function(nums) {
-    let map = new Map();
-    const result = [];
-    
-    for (let num of nums) {
-        map.set(num, (map.get(num) || 0) + 1);
-    }
-    
-    map.forEach((val, key) => {
-        if(val > Math.floor(nums.length / 3)) {
-            result.push(key);
-        }
-    })
-    
-    
-    
-    return result;
+    let n = nums.length;
+    let limit = Math.floor(n / 3);
+    const map = nums.reduce((acc, e) => acc.set(e, (acc.get(e) || 0) + 1), new Map());
+    let occurences = [...map.values()], vals = [...map.keys()];
+    vals = vals.filter((num, idx) => occurences[idx] > limit);
+    return vals;
 };
