b95e90f45748c36cbb1789cea7cdfc5cbfda18c0
SHWsgithub <111102006+SHWsgithub@users.noreply.github.com>
2023-02-20 16:36:40 +0900

    Time: 111 ms (89.89%), Space: 62.2 MB (8.47%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
@@ -4,17 +4,22 @@
  * @return {number}
  */
 var findKthLargest = function(nums, k) {
-    const pivotIndex = Math.floor(Math.random() * nums.length);
-    const pivot = nums[pivotIndex];
-    const left = [], right = [];
+    
+    const recursion = (arr, k) => {
+        const pivotIndex = Math.floor(Math.random() * arr.length);
+        const pivot = arr[pivotIndex];
+        const left = [], right = [];
 
-    for (let i = 0; i < nums.length; i++) {
-        if (i === pivotIndex) continue;
-        if (nums[i] >= pivot) left.push(nums[i]);
-        else right.push(nums[i]);
+        for (let i = 0; i < arr.length; i++) {
+            if (i === pivotIndex) continue;
+            if (arr[i] >= pivot) left.push(arr[i]);
+            else right.push(arr[i]);
+        }
+
+        if (left.length === k - 1) return pivot;
+        else if (left.length >= k) return recursion(left, k);
+        else return recursion(right, k - left.length - 1);
     }
-
-    if (left.length === k - 1) return pivot;
-    else if (left.length >= k) return findKthLargest(left, k);
-    else return findKthLargest(right, k - left.length - 1);
+    
+    return recursion(nums, k);
 };
