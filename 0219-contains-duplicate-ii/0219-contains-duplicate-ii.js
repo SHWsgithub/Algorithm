ff225277072c06de0c676c529149a01676fa12ff
SHWsgithub <111102006+SHWsgithub@users.noreply.github.com>
2023-02-21 11:28:14 +0900

    Time: 95 ms (96.72%), Space: 65.8 MB (31.25%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -4,16 +4,15 @@
  * @return {boolean}
  */
 var containsNearbyDuplicate = function(nums, k) {
-    const map = new Map();
-    
-    for(let i = 0; i < nums.length; i++) {
-        if(!map.has(nums[i])) {
-            map.set(nums[i], i);
-            continue;
-        };
-        if(i - map.get(nums[i]) <= k) return true;
-        map.set(nums[i], i);
+  const map = new Map();
+  
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    if (map.has(num) && i - map.get(num) <= k) {
+      return true;
     }
-    
-    return false;
+    map.set(num, i);
+  }
+  
+  return false;
 };
