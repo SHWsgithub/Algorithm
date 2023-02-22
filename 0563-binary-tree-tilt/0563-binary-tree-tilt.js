7911e471a4c6e2c7fe6e7f6092eadc9a42b6d5aa
SHWsgithub <111102006+SHWsgithub@users.noreply.github.com>
2023-02-22 10:04:27 +0900

    Time: 100 ms (30.64%), Space: 47.4 MB (23.39%) - LeetHub

---
+++ 0563-binary-tree-tilt/0563-binary-tree-tilt.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var findTilt = function(root) {
+      let tiltSum = 0;
+      const dfs = (root) => {
+        if (!root) {
+          return 0;
+        }
+        let left = dfs(root.left);
+        let right = dfs(root.right);
+        tiltSum += Math.abs(left - right);
+        return left + right + root.val;
+      }
+      dfs(root);
+      return tiltSum;
+};
